56eb529990a9f49a86d4d5dafebdac0e51a4eb86
MarMsa <73688209+MarMsa@users.noreply.github.com>
2024-03-11 19:00:35 +0500

    upd4

--- src/view/point-edit.js
+++ src/view/point-edit.js
@@ -19,13 +19,11 @@ function createPointPhotos(pointDestination){
 }
 
 function createPointTypes(currentType){
-
   return TYPES.map((type) =>
-  `<div class="event__type-item">
-  <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}"${currentType === type ? 'checked' : ''}>
-  <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type}</label>
-  </div>`).join('');
-
+    `<div class="event__type-item">
+    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}"${currentType === type ? 'checked' : ''}>
+    <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type}</label>
+    </div>`).join('');
 }
 
 function createPointOffers (pointOffers){
